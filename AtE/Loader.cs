1963129a904face0050fed93b3c5bfaf823320ea
Jesse Granger <jesse_granger@fastmail.com>
2022-10-26 16:28:50 -0700

    moved Settings window to Settings.cs

--- AtE/Loader.cs
+++ AtE/Loader.cs
@@ -14,56 +14,18 @@ namespace AtE {
 
 		static void Main() {
 
+			// Set up the transparent overly (with ImGui to draw on top of it)
 			Overlay.Initialise();
-			// RenderForm.IsTransparent = false;
 
-			// Plan some tasks that will run forever
+			// Enable the Setting window
+			OnRelease(Keys.F12, Settings.Toggle);
+			RunForever("Settings Window", Settings.Render);
 
-			float fpsCap = 30f;
-			bool enforceFpsCap = true;
-			{
-				bool enabled = true;
-				Run((self, dt) => {
-					if ( !enabled ) return null;
-					ImGui.Begin("Settings", ref enabled);
-					ImGui.Checkbox("", ref enforceFpsCap);
-					ImGui.SameLine();
-					ImGui.SliderFloat("FPS Cap", ref fpsCap, 10, 60);
-					ImGui.Checkbox("VSync", ref D3DController.VSync);
-					if( ImGui.Button("Test") ) {
-						long endTime = Time.ElapsedMilliseconds + 15000;
-						Run((self2, dt2) => {
-							if ( Time.ElapsedMilliseconds > endTime ) return null;
-							// ImGui.Text("Hello World!");
-							DrawText("Goodbye World!", new Vector2(100, 100), Color.Orange);
+			RunForever("Demo Window", ImGui.ShowDemoWindow);
 
-							DrawCircle(new Vector2(120, 120), 20, Color.Red);
+			RunForever("PoEMemory", PoEMemory.OnTick);
 
-							DrawFrame(new Vector2(200, 200), new Vector2(240, 240), Color.Orange);
-							return self2;
-						});
-					}
-					ImGui.SameLine();
-					if( ImGui.Button("Exit") ) {
-						Overlay.Close();
-					}
-					ImGui.End();
-					return self;
-				});
-			}
-
-			{
-				bool enabled = true;
-				Run((self, dt) => {
-					if ( !enabled ) return null;
-					double fps = dt == 0 ? 999d : 1000f / dt;
-					ImGui.Begin("FPS", ref enabled);
-					ImGui.Text($"FPS: {fps:F1}");
-					ImGui.End();
-					return self;
-				});
-			}
-
+			// Render until we drop
 			Overlay.RenderLoop();
 
 		}
