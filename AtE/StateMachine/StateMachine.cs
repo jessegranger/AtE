c21830900eb806abdbc0ac1b9c97fd2d1e6a464a
Jesse Granger <jesse_granger@fastmail.com>
2022-10-30 14:38:59 -0700

    clean out a lot of the cruft, add doc strings

--- AtE/StateMachine/StateMachine.cs
+++ AtE/StateMachine/StateMachine.cs
@@ -25,114 +25,102 @@ namespace AtE {
 				}
 			});
 		}
+		// TODO: RunWhile
 	}
 
 	internal class StateMachine : State {
 
+		public bool Paused = false;
+
+		public StateMachine(params IState[] states) {
+			States = new LinkedList<IState>(states);
+			Next = this; // state machines are states that run forever, unless you manually assign Next
+		}
+
+		// there is one default static machine that runs all the other machinery
 		public static readonly StateMachine DefaultMachine = new StateMachine();
 
-		// each machine runs any number of states at once (in 'parallel' frame-wise)
-		// when a machine is empty, it gets collected by the reaper
-		public LinkedList<IState> States;
+		// each machine runs any number of states at once
+		public readonly LinkedList<IState> States; // OnTick below will tick the states in this list, and manage the results
 
-		public IState CurrentState => States.FirstOrDefault();
 
-		public StateMachine(params IState[] states) => States = new LinkedList<IState>(states);
+		public override string ToString() => GetType().Name + "[" + string.Join(",", States.Select(s => s.Name)) + "]";
 
-		public override string ToString() => string.Join(" while ", States.Select(s => $"({s})")) + (Next == null ? "" : $" then {Next.Name}");
-
+		/// <summary>
+		/// Remove an item while iterating a linked list and continue the iteration.
+		/// </summary>
 		private static LinkedListNode<T> RemoveAndContinue<T>(LinkedList<T> list, LinkedListNode<T> node) {
 			LinkedListNode<T> next = node.Next;
 			list.Remove(node);
 			return next;
 		}
 
-		/// <summary>
-		/// Clear is a special state that clears all running states from a MultiState.
-		/// </summary>
-		/// <example>new StateMachine(
-		///   new WalkTo(X),
-		///   new ShootAt(Y, // will walk and shoot at the same time, when shoot is finished, clear the whole machine (cancel the walk)
-		///     new StateMachine.Clear(this)) );
-		///  </example>
-		public class Clear : State {
-			public Clear(State next) : base(next) { }
-			public override IState OnTick(long dt) => Next;
-		}
-
 		private Action<string> logDelegate;
 		public void EnableLogging(Action<string> logger) => logDelegate = logger;
 		public void DisableLogging() => logDelegate = null;
-		private void Log(string s) => logDelegate?.Invoke(s);
+		private void Log(string s) => logDelegate?.Invoke(GetType().Name + s);
 
-		public void Pause() => Paused = true;
-		public void Resume() => Paused = false;
-		public void TogglePause() => Paused = !Paused;
-		private bool Paused = false;
 
 		public override IState OnTick(long dt) {
-			ImGui.Begin("StateMachine");
-			// Each State in the States list will be ticked each frame
-			try {
-				if ( Paused ) {
-					return this;
-				}
-				float targetDeltaTime = 1000f / CoreSettings.FpsCap.Value;
-				// iterate over the linked list of currently active states:
-				LinkedListNode<IState> curNode = States.First;
-				while ( curNode != null ) {
-					try {
-						// each node in the linked list contains one State
-						IState curState = curNode.Value;
-						// that state is ticked once per frame
-						ImGui.End();
-						var tickStart = Time.Elapsed;
-						IState gotoState = curState.OnTick(dt);
-						var elapsed = Time.Elapsed - tickStart;
-						ImGui.Begin("StateMachine");
-						float share = (float)elapsed.TotalSeconds / targetDeltaTime;
-						ImGui.ProgressBar(share, new Vector2(250f, 0f), curState.Name);
-						ImGui.SameLine();
-						if( ImGui.Button($"X##{curState.Name}") ) {
-							gotoState = null;
-						}
-						/*
-						if ( elapsed > 100 ) {
-							Log($"OnTick: {curState.Name} took {elapsed} ms, cancelling...");
-							gotoState = null;
-						}
-						*/
+			// Each State in the States list will be ticked once each frame
+			if ( Paused ) {
+				return this;
+			}
+			// iterate over the linked list of currently active states:
+			LinkedListNode<IState> curNode = States.First;
+			while ( curNode != null ) {
+				try {
+					// each node in the linked list contains one State
+					IState curState = curNode.Value;
+					// that state is ticked once per frame
+					var tickStart = Time.Elapsed;
+					IState gotoState = curState.OnTick(dt);
+					var elapsed = Time.Elapsed - tickStart;
 
-						// the result can either terminate, replace, or continue, the State in this node
-						if ( gotoState == null ) { // terminate the State in this node
-							Log($"State Finished: {curState.Name}.");
-							curNode = RemoveAndContinue(States, curNode); // unlink from the linked list
-							continue;
-						}
-						if ( gotoState != curState ) { // replace the State in this node with gotoState
-							gotoState = gotoState.OnEnter();
-							Log($"State Changed: {curState.Name} to {gotoState.Name}");
-							if ( gotoState.GetType() == typeof(Clear) ) {
-								Cancel(except: curState); // call all OnAbort in State, except curState.OnAbort, because it just ended cleanly (as far as it knows)
-								return gotoState.Next ?? Next;
-							}
-							curState.Next = null; // just in case
-							curNode.Value = gotoState;
-						}
-					} catch ( Exception e ) {
-						Log(e.Message);
-						Log(e.StackTrace);
-						throw;
+					// the result, gotoState can either terminate, replace, or continue, the current state
+
+					// terminate
+					if ( gotoState == null ) {
+						Log($"State Finished: {curState.Name}.");
+						curNode = RemoveAndContinue(States, curNode); // unlink from the linked list
+						continue;
 					}
-					curNode = curNode.Next; // loop over the whole list
+
+					// replace
+					if ( gotoState != curState ) {
+						gotoState = gotoState.OnEnter();
+						Log($"State Changed: {curState.Name} to {gotoState.Name}");
+						curState.Next = null; // remove the forward reference from the old state
+						curNode.Value = gotoState; // gotoState often has captured it, and brings it back, but that's up to the States
+						// fall through to continue
+					}
+					// continue
+					
+				} catch ( Exception e ) {
+					Log(e.Message);
+					Log(e.StackTrace);
+					throw;
 				}
-			} finally {
-				ImGui.End();
+				curNode = curNode.Next; // loop over the whole list
 			}
 			return States.Count == 0 ? Next : this;
 		}
-		public void Cancel(IState except = null) {
+		/// <summary>
+		/// Call state.OnCancel() and remove it from this StateMachine.
+		/// </summary>
+		/// <param name="state"></param>
+		public void Cancel(IState state) {
+			state.OnCancel();
+			Remove(state);
+		}
+		public void CancelExcept(IState except = null) {
 			foreach ( IState s in States.Where(s => s != except) ) {
+				s.OnCancel();
+			}
+			States.Clear();
+		}
+		public void CancelAll() {
+			foreach ( IState s in States ) {
 				s.OnCancel();
 			}
 			States.Clear();
@@ -141,7 +129,13 @@ namespace AtE {
 			if ( state == null ) return;
 			Log($"State Added: {state.Name}");
 			States.AddLast(States.Count == 0 ? state.OnEnter() : state);
+			// call OnEnter if it's the first state bc it will start immediately
 		}
+		/// <summary>
+		/// Remove immediately without cancelling.
+		/// state.OnCancel() is not called.
+		/// </summary>
+		/// <param name="state"></param>
 		public void Remove(IState state) {
 			if ( state != null ) States.Remove(state);
 		}
