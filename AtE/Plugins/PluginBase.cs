0ef79fc341b3b61bbdac4a12c75afce00f1b56ab
Jesse Granger <jesse_granger@fastmail.com>
2022-11-01 10:03:23 -0700

    tons of fixes, more of a working Plugin system by now

--- AtE/Plugins/PluginBase.cs
+++ AtE/Plugins/PluginBase.cs
@@ -1,200 +1,155 @@
 ﻿using ImGuiNET;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static AtE.Globals;
 
 namespace AtE {
+
+	/// <summary>
+	/// Apply this attribute to plugin fields that should not persist in the Settings INI file.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field)] public class NoPersist : Attribute { }
+
+	public static partial class Globals {
+		public static T GetPlugin<T>() where T : PluginBase => PluginBase.GetPlugin<T>();
+	}
+
+	/// <summary>
+	/// All plugins must inherit from this base.
+	/// The static portion of PluginBase manages the set of plugins, and save/load settings.ini
+	/// The non-static portion defines the base members for all plugins.
+	/// </summary>
 	public class PluginBase : State {
 
-		protected static void Register<T>() where T : PluginBase, new() {
-			var t = new T();
-			Instances.Add(typeof(T).Name, t);
-			Machine.Add(t);
-			Log($"Plugin Added: {typeof(T).Name}");
-		}
-
-		private static Dictionary<string, PluginBase> Instances = new Dictionary<string, PluginBase>();
-		internal static IEnumerable<PluginBase> Plugins => Instances.Values;
-
-		internal static StateMachine Machine = new StateMachine();
-		static PluginBase() {
-			Register<PluginLoader>();
-			Register<CoreSettings>();
-			Register<ExamplePlugin>(); // later, PluginLoader should call all the right Register<T>()
-		}
-
-		public bool Enabled = true;
-		public virtual int SortIndex => int.MaxValue;
-
-		public virtual void Render() {
-			ImGui.Checkbox("Enabled", ref Enabled);
-		}
-
-		public static T GetPlugin<T>() where T : PluginBase => Instances.TryGetValue(typeof(T).Name, out PluginBase value) ? (T)value : null;
-
-	}
-
-	public class PluginLoader : PluginBase {
-		// a plugin that can download complile and load other plugins
-
-		public override int SortIndex => 1;
-		public override string Name => "Plugin Loader";
-		public override IState OnTick(long dt) {
-			if ( !Enabled ) {
-				return this; // keep it in the PluginBase.Machine, but dont run
+		private static bool TryRegisterPluginType(Type T) {
+			if ( T == null || !T.IsSubclassOf(typeof(PluginBase)) ) {
+				return false;
 			}
-			// TODO:
-			return this;
-		}
-	}
-
-	public class CoreSettings : PluginBase {
-
-		public new const bool Enabled = true; // always true for CoreSettings
-		public override int SortIndex => 0;
-		public float FPS_Maximum = 40f;
-		public bool Enable_FPS_Maximum = true;
-		public bool ShowLogWindow = false;
-
-		public override string Name => "Core Settings";
-
-		public override void Render() {
-
-			ImGui.Checkbox("Show Log Window", ref ShowLogWindow);
-			ImGui.Checkbox("FPS Cap:", ref Enable_FPS_Maximum);
-			ImGui.SameLine();
-			ImGui.SliderFloat("", ref FPS_Maximum, 10, 100);
-			if( ImGui.BeginChildFrame(1234, new Vector2(0f, -1f), ImGuiWindowFlags.AlwaysAutoResize) ) {
-					ImGui.AlignTextToFramePadding();
-				ImGui.Text($"Current FPS: {Overlay.FPS:F0}fps");
-				var target = PoEMemory.Target;
-				ImGui.AlignTextToFramePadding();
-				ImGui.Text($"Target Process: {IsValid(target)}");
-				if ( IsValid(target) ) {
-					ImGui.SameLine();
-					ImGui.AlignTextToFramePadding();
-					ImGui.Text($"PID: {target.Id}");
-					ImGui.SameLine();
-					ImGui.AlignTextToFramePadding();
-					ImGui.Text($"Handle: {PoEMemory.Handle}");
-					var root = PoEMemory.GameRoot;
-					ImGui_Address(root.Address, "Game Root:");
-					ImGui.SameLine();
-					if( ImGui.Button("B##GameRoot") ) {
-						Run_ObjectBrowser("GameRoot", root);
-					}
-					if ( Win32.GetWindowRect(target.MainWindowHandle, out var rect) ) {
-						ImGui.Text($"Window: {rect.Width}x{rect.Height} at {rect.Top},{rect.Left} ");
-					}
-				}
-				ImGui.EndChildFrame();
-			}
-		}
-
-		public override IState OnTick(long dt) {
-			if( ShowLogWindow && ImGui.Begin("Log Window", ref ShowLogWindow) ) {
-				ImGui.Text("This is the log window");
-				ImGui.End();
-			}
-			return base.OnTick(dt);
-		}
-
-	}
-
-
-	public class ExamplePlugin : PluginBase {
-
-
-		public bool UseLifeFlask = true;
-		public float UseLifeFlaskAtPctLife = 50f;
-
-		public bool ShowFlaskStatus = false;
-
-		public override string Name => "Example";
-
-		public override void Render() {
-			base.Render();
-
-			ImGui.Checkbox("Use Life Flask at %:", ref UseLifeFlask);
-			ImGui.SameLine();
-			ImGui.SliderFloat("", ref UseLifeFlaskAtPctLife, 10f, 90f);
-			ImGui.Checkbox("Show Flask Status", ref ShowFlaskStatus);
-			ImGui.SameLine();
-			if( ImGui.Button("B##browse-flasks") ) {
-				Run_ObjectBrowser("Flasks", GetUI()?.Flasks);
-			}
-		}
-		
-		public override IState OnTick(long dt) {
-			if ( !Enabled ) {
-				return this; // keep it in the PluginBase.Machine, but dont run
-			}
-
-			if( ShowFlaskStatus ) {
-				uint id = (uint)GetHashCode();
-				foreach(var elem in GetUI().Flasks.Flasks) {
-					var ent = elem.Item;
-					var rect = elem.GetClientRect();
-					var color = ent.CurCharges < ent.ChargesPerUse ? Color.Red :
-						ent.IsBuffActive ? Color.Gray :
-						Color.Green;
-					DrawFrame(rect, color, 2);
-				}
-			}
-
-			var player = GetPlayer();
-			if( IsValid(player) ) {
-				var life = player.GetComponent<Life>();
-				if( IsValid(life) ) {
-					if ( UseLifeFlask ) {
-						int maxHp = life.MaxHP - life.TotalReservedHP;
-						// 0 - 100, to match the scale of the SliderFloat above
-						float pctHp = 100 * life.CurHP / maxHp;
-						if ( pctHp < UseLifeFlaskAtPctLife ) {
-							FlaskEntity useFlask = null;
-							foreach ( FlaskEntity flask in GetFlasks() ) {
-								if( !IsValid(flask) ) {
-									// DrawTextAt(player, $"Flask {flask.FlaskIndex}: Not a valid entity.", Color.Red);
-									continue;
-								}
-								if( flask.LifeHealAmount <= 0 ) {
-									// DrawTextAt(player, $"Flask {flask.FlaskIndex}: Not a life flask.", Color.White);
-									continue;
-								}
-								if( flask.CurCharges < flask.ChargesPerUse ) {
-									// DrawTextAt(player, $"Flask {flask.FlaskIndex}: Not enough charge {flask.CurCharges} < {flask.ChargesPerUse} (of {flask.MaxCharges}).", Color.White);
-									continue;
-								}
-								if( flask.IsBuffActive ) {
-									// DrawTextAt(player, $"Flask {flask.FlaskIndex}: Already active.", Color.White);
-									continue;
-								}
-								if( flask.IsInstant || (flask.IsInstantOnLowLife && pctHp < 50) ) {
-									useFlask = flask;
-									break;
-								} else {
-									useFlask = useFlask ?? flask;
-								}
-							}
-
-							if( useFlask != null ) {
-								// DrawTextAt(GetPlayer(), $"Would use a flask {useFlask.Key}", Color.White);
-								return new PressKey(useFlask.Key, 40, new Delay(100, this));
-							}
+			Log($"Loading plugin type: {T.Name}");
+			// make a new T();
+			var constructor = T.GetConstructor(new Type[] { });
+			var obj = (PluginBase)constructor.Invoke(new object[] { });
+			if ( iniSettings.TryGetValue(T.Name, out var data) ) {
+				foreach ( var field in T.GetFields()
+					.Where(f => f.GetCustomAttribute<NoPersist>() == null) ) {
+					if ( data.TryGetValue(field.Name, out string strValue) ) {
+						if ( field.FieldType == typeof(HotKey) ) {
+							field.SetValue(obj, HotKey.Parse(strValue));
+						} else if ( field.FieldType == typeof(Keys) ) {
+							field.SetValue(obj, Enum.TryParse(strValue, out Keys key) ? key : Keys.None);
+						} else if ( field.FieldType == typeof(float) ) {
+							field.SetValue(obj, float.Parse(strValue));
+						} else if ( field.FieldType == typeof(double) ) {
+							field.SetValue(obj, double.Parse(strValue));
+						} else if ( field.FieldType == typeof(long) ) {
+							field.SetValue(obj, long.Parse(strValue));
+						} else if ( field.FieldType == typeof(int) ) {
+							field.SetValue(obj, int.Parse(strValue));
+						} else if ( field.FieldType == typeof(bool) ) {
+							field.SetValue(obj, bool.Parse(strValue));
+						} else if ( field.FieldType == typeof(string) ) {
+							field.SetValue(obj, strValue);
+						} else {
+							throw new ArgumentException($"Unknown Field Type, cannot load from string: {field.FieldType}");
 						}
 					}
 				}
 			}
-			return base.OnTick(dt);
+		
+			Instances.Add(T.Name, obj);
+			Machine.Add(obj);
+			Log($"Plugin Added: {T.Name}");
+			return true;
+		}
+
+		private static Dictionary<string, PluginBase> Instances = new Dictionary<string, PluginBase>();
+		internal static IEnumerable<PluginBase> Plugins => Instances.Values;
+		internal static T GetPlugin<T>() where T : PluginBase => Instances.TryGetValue(typeof(T).Name, out PluginBase value) ? (T)value : null;
+
+		internal static StateMachine Machine = new StateMachine();
+		static PluginBase() {
+			LoadIniFile();
+
+			foreach(var pluginType in typeof(PluginBase).Assembly.GetTypes() ) {
+				TryRegisterPluginType(pluginType);
+			}
+		
+		}
+
+		private static readonly string SettingsFileName = "Settings.ini";
+		private static Dictionary<string, Dictionary<string, string>> iniSettings = new Dictionary<string, Dictionary<string, string>>();
+		private static void LoadIniFile() {
+			if ( !File.Exists(SettingsFileName) ) {
+				File.Create(SettingsFileName);
+			} else {
+				Log($"Parsing ini file...");
+				string sectionName = "";
+				foreach ( var line in File.ReadAllLines(SettingsFileName) ) {
+					if ( line.StartsWith("[") ) {
+						sectionName = line.Substring(1, line.Length - 2);
+						if ( !iniSettings.ContainsKey(sectionName) ) {
+							iniSettings[sectionName] = new Dictionary<string, string>();
+						}
+					} else {
+						var key_value = line.Split('=');
+						if ( key_value.Length > 1 ) {
+							Log($"Parsed {key_value[0]}");
+							iniSettings[sectionName][key_value[0]] = string.Join("=", key_value.Skip(1));
+						}
+					}
+				}
+			}
+		}
+
+		internal static void SaveIniFile() {
+			StringBuilder sb = new StringBuilder();
+			foreach(var plugin in Instances.Values.OrderBy(p => p.SortIndex) ) {
+				sb.AppendLine($"[{plugin.GetType().Name}]");
+				foreach ( var field in plugin.GetType().GetFields() ) {
+					if ( field.GetCustomAttribute<NoPersist>() != null ) continue;
+					sb.AppendLine($"{field.Name}={field.GetValue(plugin)}");
+				}
+				sb.AppendLine();
+			}
+			string text = sb.ToString();
+			Log($"Saving INI File: {text}");
+			File.WriteAllText(SettingsFileName, text);
 		}
 
 
+		/// <summary>
+		/// A disabled Plugin does have have it's OnTick() called, and can take no actions.
+		/// It's settings page will still call Render().
+		/// </summary>
+		public bool Enabled = true; // Enabled is persistent, saved to profile settings
+
+		/// <summary>
+		/// Paused it meant to be toggled via HotKey, and temporarily pauses some Plugin functions.
+		/// The Plugin remains enabled, and it's OnTick() will be called, but should determine it's own
+		/// appropriate response to Paused being true.
+		/// </summary>
+		[NoPersist]
+		public bool Paused = false; // Paused is temporary, not saved to profile settings
+
+		/// <summary>
+		/// Implementation of PluginBase can override this SortIndex to put themselves higher up in the list.
+		/// You probably don't need this, just the first few Core-ish plugins want to be at the top.
+		/// </summary>
+		public virtual int SortIndex => int.MaxValue;
+
+		/// <summary>
+		/// Called when the Console needs to render the settings panel for this plugin.
+		/// </summary>
+		public virtual void Render() {
+			ImGui.Checkbox("Enabled", ref Enabled);
+		}
 
 	}
 }
