736c575298c77590154370566c16bac2333ae6d0
Jesse Granger <jesse_granger@fastmail.com>
2022-11-09 17:42:58 -0800

    add comments to the ExamplePlugin

--- AtE/Plugins/ExamplePlugin.cs
+++ AtE/Plugins/ExamplePlugin.cs
@@ -1,32 +1,35 @@
 ﻿using ImGuiNET;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using static AtE.Globals;
 
 namespace AtE {
 	public class ExamplePlugin : PluginBase {
 
+		// public Fields of known types will be automatically persisted using an .ini file
 		public bool ShowDemoWindow = false;
+		// see: PluginBase for the supported types
 		public bool ShowMetricsWindow = false;
 
+		/// <summary>
+		/// Uses ImGui to render controls for configurable fields.
+		/// </summary>
 		public override void Render() {
 			base.Render();
 			ImGui.Checkbox("Show Demo Window", ref ShowDemoWindow);
 			ImGui.Checkbox("Show Metrics Window", ref ShowMetricsWindow);
 		}
 
+		/// <summary>
+		/// This is run every frame.
+		/// </summary>
+		/// <param name="dt">duration of this frame, in ms</param>
+		/// <returns>This plugin, or another IState to replace it.</returns>
 		public override IState OnTick(long dt) {
-			if ( Paused || !Enabled ) return this;
-			if( ShowDemoWindow ) {
-				ImGui.ShowDemoWindow();
-			}
-			if( ShowMetricsWindow ) {
-				ImGui.ShowMetricsWindow();
+			if ( Enabled && !Paused ) {
+				if ( ShowDemoWindow ) {
+					ImGui.ShowDemoWindow();
+				}
+				if ( ShowMetricsWindow ) {
+					ImGui.ShowMetricsWindow();
+				}
 			}
 			return this;
 		}
