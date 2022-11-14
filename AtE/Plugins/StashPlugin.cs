4ee40ca734f35c09a2c0a0e122d0e5762a98128a
Jesse Granger <jesse_granger@fastmail.com>
2022-11-13 19:30:46 -0800

    start a StashPlugin, empty so far

---
+++ AtE/Plugins/StashPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AtE.Globals;
+
+namespace AtE.Plugins {
+	public class StashPlugin : PluginBase {
+
+		public override string Name => "Stash";
+
+
+	}
+}
