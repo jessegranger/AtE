cf4cb31cc75b7c0e4c0f66a4bb710e3afc80a58e
Jesse Granger <jesse_granger@fastmail.com>
2022-11-19 09:06:37 -0800

    be willing to use an instant life flask while a slow one is in effect; consider a rare hurt only when it's Life is missing

--- AtE/Plugins/FlasksPlugin.cs
+++ AtE/Plugins/FlasksPlugin.cs
@@ -26,7 +26,6 @@ namespace AtE {
 		// Feature: Highlight flasks with a colored status border
 		public bool ShowFlaskStatus = false;
 
-		public bool ShowDebugging = false;
 
 		public override string Name => "Flasks";
 
@@ -67,7 +66,7 @@ namespace AtE {
 
 		public bool IsUsable(FlaskEntity flaskEnt) => IsValid(flaskEnt)
 			&& flaskEnt.Charges_Cur >= flaskEnt.Charges_Per
-			&& (Time.ElapsedMilliseconds - mostRecentFlask) > 150 // a global throttle on all flasks, so it will only use one at a time
+			&& (Time.ElapsedMilliseconds - mostRecentFlask) > 170 // a global throttle on all flasks, so it will only use one at a time
 			&& (Time.ElapsedMilliseconds - mostRecentUse[flaskEnt.FlaskIndex]) > 250 // a throttle on each flask
 			;
 
@@ -95,7 +94,7 @@ namespace AtE {
 				return this; // keep it in the PluginBase.Machine, but dont do anything
 			}
 
-			if( ! PoEMemory.IsAttached ) {
+			if ( !PoEMemory.IsAttached ) {
 				return this;
 			}
 
@@ -117,7 +116,13 @@ namespace AtE {
 				return this;
 			}
 
-			if ( HasBuff(GetPlayer(), "grace_period") ) {
+			var player = GetPlayer();
+			if ( !IsValid(player) ) {
+				return this;
+			}
+
+			var buffs = player.GetComponent<Buffs>();
+			if ( HasBuff(buffs, "grace_period") ) {
 				return this;
 			}
 
@@ -137,11 +142,6 @@ namespace AtE {
 			}
 			FlaskEntity flaskToUse = null;
 
-			var player = GetPlayer();
-			if ( !IsValid(player) ) {
-				return this;
-			}
-
 			var life = player.GetComponent<Life>();
 			if ( IsValid(life) ) {
 				// Feature: Use Life flask
@@ -150,11 +150,11 @@ namespace AtE {
 					// 0 - 100, to match the scale of the SliderFloat above
 					float pctHp = 100 * life.CurHP / maxHp;
 					if ( pctHp < UseLifeFlaskAtPctLife ) {
-						foreach ( FlaskEntity flask in GetFlasks().Where(IsUsable).Where(f => f.LifeHealAmount > 0 && !f.IsBuffActive) ) {
+						foreach ( FlaskEntity flask in GetFlasks().Where(IsUsable).Where(f => f.LifeHealAmount > 0) ) {
 							if ( flask.IsInstant || (flask.IsInstantOnLowLife && pctHp < 50) ) {
 								flaskToUse = flask;
 								break;
-							} else {
+							} else if ( !flask.IsBuffActive ) {
 								flaskToUse = flaskToUse ?? flask;
 							}
 						}
@@ -185,12 +185,11 @@ namespace AtE {
 
 			// Feature: Cure Conditions
 			if ( CureConditions ) {
-				var buffs = player.GetComponent<Buffs>();
 				if ( IsValid(buffs) ) {
 					if ( HasBuff(buffs, "frozen") && TryGetUsableFlask(f => f.Cures_Frozen, out flaskToUse) ) {
 						return UseFlask(flaskToUse);
 					}
-					if ( HasBuff(buffs, "bleeding") && TryGetUsableFlask(f => f.Cures_Bleeding, out flaskToUse) ) {
+					if ( (HasBuff(buffs, "bleeding") || HasBuff(buffs, "corrupted_blood")) && TryGetUsableFlask(f => f.Cures_Bleeding, out flaskToUse) ) {
 						return UseFlask(flaskToUse);
 					}
 					if ( HasBuff(buffs, "poisoned") && TryGetUsableFlask(f => f.Cures_Poisoned, out flaskToUse) ) {
@@ -214,18 +213,16 @@ namespace AtE {
 			}
 
 			if ( UseUtilityWhenHitRareOrUnique ) {
-				bool hasHitNearbyRare = GetEntities().Where(e =>
-					// Only Enemies:
-					IsValid(e) && e.IsHostile
+				bool hasHitNearbyRare = GetEnemies().Where(e =>
 					// Rare or greater:
-					&& e.GetComponent<ObjectMagicProperties>()?.Rarity >= Offsets.MonsterRarity.Rare)
+					e.GetComponent<ObjectMagicProperties>()?.Rarity >= Offsets.MonsterRarity.Rare)
 					// If there are any hurt
 					.Any(e => {
 						var e_life = e.GetComponent<Life>();
-						return IsValid(e_life) && (e_life.CurHP + e_life.CurES) < (e_life.MaxHP + e_life.MaxES);
+						return IsValid(e_life) && e_life.CurHP > 0 && e_life.CurHP < e_life.MaxHP;
 					});
 				if ( hasHitNearbyRare ) {
-					if ( TryGetUsableFlask(f => f.LifeHealAmount == 0 
+					if ( TryGetUsableFlask(f => f.LifeHealAmount == 0
 						&& f.ManaHealAmount == 0
 						&& !f.IsBuffActive
 						, out flaskToUse) ) {
