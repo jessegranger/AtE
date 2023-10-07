1d076476d898c6071336c924960b2148ca0d0550
Jesse Granger <jesse_granger@fastmail.com>
2023-10-07 11:35:11 -0700

    Keyboard: allow more than one keybinding

--- AtE/Plugins/MouseKeyboardPlugin.cs
+++ AtE/Plugins/MouseKeyboardPlugin.cs
@@ -19,14 +19,26 @@ namespace AtE {
 
 		public bool EnableAlsoCast = false;
 		public HotKey AlsoCastMainKey = new HotKey(Keys.None);
-		public bool EnableAlsoCastAlways = false;
-		public HotKey AlsoCastAlwaysKey = new HotKey(Keys.None);
-		public bool EnableAlsoCast4Second = false;
-		public HotKey AlsoCast4SecondKey = new HotKey(Keys.None);
-		public bool EnableAlsoCast8Second = false;
-		public HotKey AlsoCast8SecondKey = new HotKey(Keys.None);
-		public bool EnableAlsoCast16Second = false;
-		public HotKey AlsoCast16SecondKey = new HotKey(Keys.None);
+
+		public bool EnableAlsoCastKey1 = false;
+		public HotKey AlsoCastKey1 = new HotKey(Keys.None);
+		public int AlsoCastKey1Throttle = 0;
+		private long LastPressKey1 = 0;
+
+		public bool EnableAlsoCastKey2 = false;
+		public HotKey AlsoCastKey2 = new HotKey(Keys.None);
+		public int AlsoCastKey2Throttle = 0;
+		private long LastPressKey2 = 0;
+
+		public bool EnableAlsoCastKey3 = false;
+		public HotKey AlsoCastKey3 = new HotKey(Keys.None);
+		public int AlsoCastKey3Throttle = 0;
+		private long LastPressKey3 = 0;
+
+		public bool EnableAlsoCastKey4 = false;
+		public HotKey AlsoCastKey4 = new HotKey(Keys.None);
+		public int AlsoCastKey4Throttle = 0;
+		private long LastPressKey4 = 0;
 
 		public bool ShowMouseCoords = false;
 		public HotKey ResetZoneSecretKey = new HotKey(Keys.None);
@@ -37,7 +49,9 @@ namespace AtE {
 			ImGui.Text("Mouse:");
 			ImGui.Checkbox("Show Cursor Position", ref ShowMouseCoords);
 			ImGui.Checkbox("Repeat Left Clicks", ref RepeatLeftClicks);
+			ImGui.SetNextItemWidth(200f);
 			ImGui.SliderInt("Start repeating after", ref RepeatLeftClickWait, 300, 1000);
+			ImGui.SetNextItemWidth(200f);
 			ImGui.SliderInt("Then, repeat interval", ref RepeatLeftClickInterval, 100, 300);
 			ImGui.Checkbox("While Holding Shift", ref OnlyWhileHoldingShift);
 			ImGui.Checkbox("While Holding Control", ref OnlyWhileHoldingControl);
@@ -50,25 +64,41 @@ namespace AtE {
 			ImGui.SameLine();
 			ImGui_HelpMarker("While you hold the main key, the other keys will cast automatically");
 
-			ImGui.Checkbox("Always##UseAlsoCast", ref EnableAlsoCastAlways);
+			ImGui.Checkbox("Also Press##AlsoCast1", ref EnableAlsoCastKey1);
 			ImGui.SameLine();
-			ImGui_HotKeyButton("Always", ref AlsoCastAlwaysKey);
-
-			/* Not Implemented yet:
-
-			ImGui.Checkbox("Every 4 Seconds##UseAlsoCast", ref EnableAlsoCast4Second);
+			ImGui_HotKeyButton("Key #1", ref AlsoCastKey1);
 			ImGui.SameLine();
-			ImGui_HotKeyButton("Every 4 Seconds", ref AlsoCastAlwaysKey);
-
-			ImGui.Checkbox("Every 8 Seconds##UseAlsoCast", ref EnableAlsoCast8Second);
+			ImGui.Text("Every");
 			ImGui.SameLine();
-			ImGui_HotKeyButton("Every 8 Seconds", ref AlsoCastAlwaysKey);
-
-			ImGui.Checkbox("Every 16 Seconds##UseAlsoCast", ref EnableAlsoCast16Second);
+			ImGui.SetNextItemWidth(100f);
+			ImGui.InputInt("seconds##Key1", ref AlsoCastKey1Throttle, 1);
+	
+			ImGui.Checkbox("Also Press##AlsoCast2", ref EnableAlsoCastKey2);
 			ImGui.SameLine();
-			ImGui_HotKeyButton("Every 16 Seconds", ref AlsoCastAlwaysKey);
-			*/
-
+			ImGui_HotKeyButton("Key #2", ref AlsoCastKey2);
+			ImGui.SameLine();
+			ImGui.Text("Every");
+			ImGui.SameLine();
+			ImGui.SetNextItemWidth(100f);
+			ImGui.InputInt("seconds##Key2", ref AlsoCastKey2Throttle, 1);
+		
+			ImGui.Checkbox("Also Press##AlsoCast3", ref EnableAlsoCastKey3);
+			ImGui.SameLine();
+			ImGui_HotKeyButton("Key #3", ref AlsoCastKey3);
+			ImGui.SameLine();
+			ImGui.Text("Every");
+			ImGui.SameLine();
+			ImGui.SetNextItemWidth(100f);
+			ImGui.InputInt("seconds##Key3", ref AlsoCastKey3Throttle, 1);
+		
+			ImGui.Checkbox("Also Press##AlsoCast4", ref EnableAlsoCastKey4);
+			ImGui.SameLine();
+			ImGui_HotKeyButton("Key #4", ref AlsoCastKey4);
+			ImGui.SameLine();
+			ImGui.Text("Every");
+			ImGui.SameLine();
+			ImGui.SetNextItemWidth(100f);
+			ImGui.InputInt("seconds##Key4", ref AlsoCastKey4Throttle, 1);
 		}
 
 		private bool downBefore = false;
@@ -127,19 +157,34 @@ namespace AtE {
 			return this;
 		}
 
+		private void doKeyDown(Keys key, ref long lastPress, int throttle) {
+			long elapsed = Time.ElapsedMilliseconds - lastPress;
+			if( elapsed < throttle * 1000 ) {
+				return;
+			}
+			lastPress = Time.ElapsedMilliseconds;
+			Win32.SendInput(Win32.INPUT_KeyDown(key));
+		}
+
+		private void doKeyUp(Keys key) {
+			Win32.SendInput(Win32.INPUT_KeyUp(key));
+		}
+
 		private bool alsoCastMainKeyDownBefore = false;
 		private void CheckAlsoCast() {
 			if( EnableAlsoCast && ! PoEMemory.GameRoot.InGameState.HasInputFocus ) {
 				bool downNow = Win32.IsKeyDown(AlsoCastMainKey.Key);
 				if( downNow && ! alsoCastMainKeyDownBefore ) {
 					// OnKeyDown:
-					if( EnableAlsoCastAlways ) {
-						Win32.SendInput(Win32.INPUT_KeyDown(AlsoCastAlwaysKey.Key));
-					}
+					if ( EnableAlsoCastKey1 ) { doKeyDown(AlsoCastKey1.Key, ref LastPressKey1, AlsoCastKey1Throttle); }
+					if ( EnableAlsoCastKey2 ) { doKeyDown(AlsoCastKey2.Key, ref LastPressKey2, AlsoCastKey2Throttle); }
+					if ( EnableAlsoCastKey3 ) { doKeyDown(AlsoCastKey3.Key, ref LastPressKey3, AlsoCastKey3Throttle); }
+					if ( EnableAlsoCastKey4 ) { doKeyDown(AlsoCastKey4.Key, ref LastPressKey4, AlsoCastKey4Throttle); }
 				} else if ( alsoCastMainKeyDownBefore && !downNow ) {
-					if( EnableAlsoCastAlways ) {
-						Win32.SendInput(Win32.INPUT_KeyUp(AlsoCastAlwaysKey.Key));
-					}
+					if( EnableAlsoCastKey1 ) { doKeyUp(AlsoCastKey1.Key); }
+					if( EnableAlsoCastKey2 ) { doKeyUp(AlsoCastKey2.Key); }
+					if( EnableAlsoCastKey3 ) { doKeyUp(AlsoCastKey3.Key); }
+					if( EnableAlsoCastKey4 ) { doKeyUp(AlsoCastKey4.Key); }
 				}
 				alsoCastMainKeyDownBefore = downNow;
 			}
